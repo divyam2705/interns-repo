623a3519b3d4f02a839d96339ab4cb11989a44f2
divyam <divyam.prakash@eximietas.com>
2024-01-12 10:35:23 +0530

    feat:Add code to the components

---
+++ myredux/src/store/slices/UserSlice.jsx
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const userSlice = createSlice({
+
+  name: "user",
+  initialState: [],
+  reducers: {
+    addUser(state, action) {
+      state.push(action.payload);
+    },
+    removeUser(state, action) {
+       state.splice(action.payload, 1)
+      
+    },
+    clearAllUsers(state, action) { 
+      return [];
+    },
+  },
+});
+
+console.log(userSlice.actions.addUser());
+
+export default userSlice.reducer;
+export const { addUser, removeUser, clearAllUsers } = userSlice.actions;
